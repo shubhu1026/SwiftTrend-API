847ec30de8b01eca3629b1c43217c22507515c6a
shubhu1026 <shubhu.p2000@gmail.com>
2024-03-21 00:56:38 -0400

    Address book changes

--- models/AddressModel.js
+++ models/AddressModel.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
 const addressSchema = new mongoose.Schema({
+  name: { type: String, required: true }, // New field: name
+  mobile: { type: String, required: true }, // New field: phone number
   street: { type: String, required: true },
   city: { type: String, required: true },
   state: { type: String, required: true },
